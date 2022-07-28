fb5b6985a856955e180644cdc849ec0fd94aaa66
Elissei Hilakivi <elissei.hilakivi@gmail.com>
2022-07-29 01:19:01 +0300

    2.5 commit 18

--- part2/kurssitiedot/src/App.js
+++ part2/kurssitiedot/src/App.js
@@ -1,43 +1,6 @@
-const Course = ({ course }) => {
-  return(
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-const Header = ({ course }) => 
-    <div>
-      <h2>{course}</h2>
-    </div>
-  
-const Content = ({ parts }) => {
-  return(
-    <>
-      {parts.map(part =>
-        <Part key={part.id}
-          part={part} />
-        )}
-    </>
-  )
-} 
-    
-const Part = ({ part }) => 
-    <p>
-      {part.name} {part.exercises}
-    </p>  
+import Course from './components/Course'
 
 
-const Total = ({ parts }) => {
-  const exs = parts.reduce((a, b) => a + b.exercises, 0) 
-  return(
-    <div>
-      <h3>Total of {exs} exercises</h3>
-    </div>
-  )
-}
 
 const App = () => {
   const courses = [
